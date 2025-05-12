7932e1d5d289c141dc1bec17bdf5b99812396ba3
adityadixit-dev <adityadixit@gmail.com>
2025-05-13 01:04:50 +0700

    Report Formatting and accepting command line arguements

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from stats import char_frequence_in_text, num_words_in_text
+import sys
+
+from stats import char_frequency_in_text, freq_dict_to_sorted_list, num_words_in_text
 
 
 def main():
-    book_file_path = "./books/frankenstein.txt"
+    # book_file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file_path = sys.argv[1]
+
     book_text = get_book_text(book_file_path)
     # print(book_text)
     num_words_in_book = num_words_in_text(book_text)
-    print(f"{num_words_in_book} words found in the document")
-    char_freq_dict = char_frequence_in_text(book_text)
-    print(char_freq_dict)
+    # print(f"{num_words_in_book} words found in the document")
+    char_freq_dict = char_frequency_in_text(book_text)
+    # print(char_freq_dict)
+    sorted_char_list = freq_dict_to_sorted_list(char_freq_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words_in_book} total words")
+    print("--------- Character Count -------")
+    for char_data in sorted_char_list:
+        if char_data["char"].isalpha():
+            print(f"{char_data['char']}: {char_data['num']}")
+    print("============= END ===============")
 
 
 def get_book_text(book_filepath):
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def num_words_in_text(text):
     return len(text.split())
 
 
-def char_frequence_in_text(text: str):
+def char_frequency_in_text(text: str):
     text = text.lower()
     char_freq_dict = {}
     for c in text:
@@ -11,3 +11,13 @@ def char_frequence_in_text(text: str):
         char_freq_dict[c] += 1
 
     return char_freq_dict
+
+
+def freq_dict_to_sorted_list(char_freq_dict):
+    list_from_dict = [{"char": k, "num": val} for k, val in char_freq_dict.items()]
+    # print(list_from_dict)
+    list_from_dict.sort(
+        reverse=True,
+        key=lambda x: x["num"],
+    )
+    return list_from_dict
